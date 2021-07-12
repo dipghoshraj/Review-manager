f1e6691d12c81eae52c0d189c592672a3ea0701e
dipghosh <dip@spotkwik.com>
2021-07-12 23:39:19 +0530

    Add the dish

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,2 +1,4 @@
 class Dish < ApplicationRecord
+    belongs_to :resturant
+    validates :resturant_id, presence: true
 end
--- db/migrate/20210712111813_create_dishes.rb
+++ db/migrate/20210712111813_create_dishes.rb
@@ -1,6 +1,8 @@
 class CreateDishes < ActiveRecord::Migration[6.1]
   def change
     create_table :dishes do |t|
+      t.belongs_to :resturant
+      t.string :name
 
       t.timestamps
     end

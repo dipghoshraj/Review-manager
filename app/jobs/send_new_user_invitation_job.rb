8fca5bac757e50472769d52d8a9ccd8beb384da7
dipghosh <dip@spotkwik.com>
2021-07-13 03:14:08 +0530

    added wait until

--- app/jobs/send_new_user_invitation_job.rb
+++ app/jobs/send_new_user_invitation_job.rb
@@ -2,6 +2,6 @@ class SendNewUserInvitationJob < ApplicationJob
   queue_as :default
 
   def perform(email)
-    UserMailer.invite(email).deliver_later(wait: 1.minutes)
+    UserMailer.invite(email).deliver_later(wait_until: 1.minutes.from_now)
   end
 end

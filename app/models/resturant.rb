e9af14ab1f756e0a1de12f2b2f1755f15e11a809
dipghosh <dip@spotkwik.com>
2021-07-12 23:43:32 +0530

    add dishs in resturant

--- app/models/resturant.rb
+++ app/models/resturant.rb
@@ -1,5 +1,7 @@
 class Resturant < ApplicationRecord
   validates_uniqueness_of :name
+  has_many :dishs, dependent: :destroy
+
 
   def as_json(opt={})
     basic_attribute = self.slice(:id, :name, :specility, :location)
